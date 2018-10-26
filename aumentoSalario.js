ad15924d1be03fe597fd345f05a7d95d97c7d817
Cristiano Gonçalves Ventura <cristainocgv@gmail.com>
2018-10-25 22:27:41 -0300

    Novo arquivo adicionado AumentoSalario.js

---
+++ aumentoSalario.js
@@ -0,0 +1,11 @@
+/* Professor Cristiano Gonçalves 
+ Calculando um novo salario. 
+ Para exercicitar basta trocar os valores das variáveis salario e aumento
+*/
+
+const salario = 3000
+const aumento = 500
+
+novoSalario = salario + aumento
+
+console.log(`O novo salário é RS${novoSalario}`)
