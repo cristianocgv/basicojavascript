907bb6b095f4479750693ea11fb52b778d95ccbd
Cristiano Gonçalves Ventura <cristainocgv@gmail.com>
2018-10-26 00:37:10 -0300

    teste 400

--- aumentoSalario.js
+++ aumentoSalario.js
@@ -1,5 +1,5 @@
 /* Professor Cristiano Gonçalves
-Exemplo 400 para updade 02
+Exemplo 500 para updade 02
  Calculando um novo salario. 
  Para exercicitar basta trocar os valores das variáveis salario e aumento
 */
